c2e38d1d195b82fee2502098b6f963f66fbb42fc
Andres Calzadias <acalzadias467@icloud.com>
2022-07-03 23:07:35 -0700

    Added some JS logic, but it isnt working

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-function makeRows() {
-    container.style.setProperty("--grid-rows", 3);
-    container.style.setProperty("--grid-cols", 4);
-    for (let i = 0; i < (3*4); i++) {
-        let cell = document.createElement("div");
-        container.appendChild(cell).className = "grid-item";
-    };
-};
+class Calculator{
+    constructor(previousOperandText, currentOperandText){
+        this.previousOperandText = previousOperandText;
+        this.currentOperandText = currentOperandText;
+    }
+    clear(){
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+    delete(){}
+    appendNumber(number){
+        if (number ==='.' && this.currentOperand.includes('.'))return
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+    chooseOperations(){}
+    compute(){}
+    updateDisplay(){
+        this.currentOperandText.innerText = this.currentOperand;
+    }
+}
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelectorAll('[data-equals]');
+const deleteButton = document.querySelectorAll('[data-delete]');
+const allClearButton = document.querySelector('[data-clear]');
+const previousOperandText = document.querySelector('[data-previous-operand]');
+const currentOperandText = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandText, currentOperandText);
+
+numberButtons.forEach(button =>{
+    button.addEventListener('click',() => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
